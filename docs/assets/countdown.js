b6e08202701abee4f38f30fe4502942b3cd9e908
Javier Sánchez <73181608+Kaputt4@users.noreply.github.com>
2023-08-28 00:38:00 +0200

    v0.4 Add day name

--- docs/assets/countdown.js
+++ docs/assets/countdown.js
@@ -1,10 +1,35 @@
 function makeTimer() {
 
-    var endTime = new Date(Date.UTC(2023, 7, 28, 4, 0, 0));
-    var endTime = Date.parse(endTime) / 1000;
-
     var now = new Date();
     var now = Date.parse(now) / 1000;
+
+    var day_28 = new Date(Date.UTC(2023, 7, 28, 4, 0, 0));
+    var day_28 = Date.parse(day_28) / 1000;
+
+    var day_2 = new Date(Date.UTC(2023, 8, 2, 4, 45, 0));
+    var day_2 = Date.parse(day_2) / 1000;
+
+    var day_3 = new Date(Date.UTC(2023, 8, 3, 4, 30, 0));
+    var day_3 = Date.parse(day_3) / 1000;
+
+    var day_2024 = new Date(Date.UTC(2024, 7, 28, 4, 0, 0));
+    var day_2024 = Date.parse(day_2024) / 1000;
+
+    var name = ""
+
+    if (now > day_3) {
+        endTime = day_2024
+        name = "&#160San Agustín 2024&#160"
+    } else if (now > day_2) {
+        endTime = day_3
+        name = "&#160el domingo 3 de sep&#160"
+    } else if (now > day_28) {
+        endTime = day_2
+        name = "&#160el sábado 2 de sep&#160"
+    } else {
+        endTime = day_28
+        name = "&#160;San Agustín&#160"
+    }
 
     var timeLeft = endTime - now;
 
@@ -21,6 +46,7 @@ function makeTimer() {
     document.getElementById("hours").innerHTML = hours + "<span>Horas</span>";
     document.getElementById("minutes").innerHTML = minutes + "<span>Minutos</span>";
     document.getElementById("seconds").innerHTML = seconds + "<span>Segundos</span>";
+    document.getElementById("day_name").innerHTML = name;
 
 }
 
